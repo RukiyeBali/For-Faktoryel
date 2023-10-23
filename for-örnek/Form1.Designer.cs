f4e33ea9c95812d8849d6e0926e90822603eda49
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-10-23 15:18:57 +0300

    Add files via upload

---
+++ for-örnek/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace for_örnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnBul = new System.Windows.Forms.Button();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(86, 163);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(52, 18);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "label1";
+            // 
+            // btnBul
+            // 
+            this.btnBul.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBul.Location = new System.Drawing.Point(70, 70);
+            this.btnBul.Name = "btnBul";
+            this.btnBul.Size = new System.Drawing.Size(100, 54);
+            this.btnBul.TabIndex = 1;
+            this.btnBul.Text = "Faktöryeli Bul";
+            this.btnBul.UseVisualStyleBackColor = true;
+            this.btnBul.Click += new System.EventHandler(this.btnBul_Click_1);
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi.Location = new System.Drawing.Point(70, 40);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 24);
+            this.txtSayi.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 229);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.btnBul);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnBul;
+        private System.Windows.Forms.TextBox txtSayi;
+    }
+}
+
---
+++ for-örnek/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_örnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btnBul_Click_1(object sender, EventArgs e)
+        {
+            int sayi1, sayi2;
+            int carpim = 1;
+
+            sayi1 = Convert.ToInt32(txtSayi.Text);
+
+
+            for (int i = 1; i <= sayi1; i++)
+            {
+                carpim *= i;
+            }
+            lbl1.Text = carpim.ToString();
+        }
+    }
+}
